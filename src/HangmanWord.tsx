e0837913f2b0c21bce53ef67ce36d70aa8dd1030
Aspen Ishii <ishii.aspen@gmail.com>
2024-05-29 14:52:25 -0600

    Add logic for word guess component

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,32 @@
 export function HangmanWord() {
-    
+    const word = "test"
+    const guessedLetters = ["t", "e", ]
+    return (
+    <div 
+        style ={{ 
+            display: "flex",
+            gap: ".25em",
+            fontSize: "6rem",
+            fontWeight: "bold",
+            textTransform: "uppercase",
+            fontFamily: "monospace",
+        }}>
+{/* render the wordToGuess by slitting it apart into its individual letters.
+note: Index is the unique id for the word and thus can be used as an exception for the key*/}
+
+            {word.split("").map((letter, index) => (
+            <span key={index} style={{ borderBottom: ".1em solid black" }}>
+                <span
+                style={{
+                    visibility: guessedLetters.includes(letter) 
+                    ? "visible" 
+                    : "hidden"
+                }}
+                >
+                {letter}
+                </span>
+            </span>
+            ))}
+    </div>
+    )
 }
